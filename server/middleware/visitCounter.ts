9fe52cf7145bc3572de7074c00538e3cf6ac4c1e
pinchao <cpc4253@163.com>
2023-08-03 14:27:12 +0800

    fix redis return different result

--- server/middleware/visitCounter.ts
+++ server/middleware/visitCounter.ts
@@ -1,6 +1,7 @@
 // 路由中间件：记录网站访问次数
 import { Request, Response, NextFunction } from 'express';
 import { redisIncrAsync } from '../db/redisClient';
+import { NODE_ENV } from '../config/config';
 
 const visitCounter = async (
   req: Request,
@@ -8,7 +9,10 @@ const visitCounter = async (
   next: NextFunction
 ) => {
   try {
-    const count = await redisIncrAsync('visitCount');
+    const isProd = NODE_ENV === 'production';
+    const count = isProd
+      ? await redisIncrAsync('visitCount')
+      : await redisIncrAsync('visitCount_dev');
     res.locals.visitCount = count;
     next();
   } catch (err) {
